b5eba60bc8693af67a7e16afa6f7484eb6455069
Akseli Leino <veksepoli@gmail.com>
2021-08-05 10:37:27 +0300

    Commenting

--- polynomDerivate.py
+++ polynomDerivate.py
@@ -1,3 +1,4 @@
+# Input
 many = int(input("How big polynom: "))+1
 
 FX = []
@@ -5,9 +6,11 @@ fx = []
 for i in range(many,0,-1):
 	FX.append(int(input(str(i-1)+"'s x: ")))
 
+# Main derivation
 for i in range(many-1):
 	fx.append((many-i-1)*FX[i])
 
+# Output
 print()
 for i in range(len(fx)-1,0,-1):
 	if fx[many-i-2] != 0:
